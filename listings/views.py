00ed23fd12a944ec87d06d04177d2b0f9bd7188e
Riddhi-Daspute <dasputeriddhi22@gmail.com>
2026-07-06 23:24:42 +0530

    Add landlord property listing form

--- listings/views.py
+++ listings/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+
+    if request.method == "POST":
+        print("Form Submitted!")
+        
     return render(request, 'home.html')
